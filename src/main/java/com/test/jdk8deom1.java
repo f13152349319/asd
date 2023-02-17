4b06fe82c6d16494468b0ad92f87dca81f41d76b
f13152349319 <f13152349319@163.com>
2023-02-17 17:22:34 +0800

    遍历集合

--- src/main/java/com/test/jdk8deom1.java
+++ src/main/java/com/test/jdk8deom1.java
@@ -1,9 +1,17 @@
 package com.test;
 
 import java.math.MathContext;
+import java.util.ArrayList;
 
 public class jdk8deom1 {
     public static void main(String[] args) {
-        System.out.println("测试");
+        ArrayList<Integer> list=new ArrayList<>();
+        for (int i=0;i<10;i++) {
+            list.add(i);
+        }
+
+        for (Integer lista:list){
+            System.out.println(lista.toString());
+        }
     }
 }
