2cdfad5d48d61ebf9377213d9776686c225c35f7
f13152349319 <f13152349319@163.com>
2023-02-17 14:56:40 +0800

    1

--- src/main/java/com/test/jdk8deom1.java
+++ src/main/java/com/test/jdk8deom1.java
@@ -4,12 +4,6 @@ import java.math.MathContext;
 
 public class jdk8deom1 {
     public static void main(String[] args) {
-        String num="";
-        StringBuilder a=new StringBuilder("123456789");
-        a.insert(1,"颤三");
-        System.out.println(a.toString());
-        int b=45;
-        System.out.println("第二次上传"+b);
-        System.out.println("第三次上传");
+
     }
 }
